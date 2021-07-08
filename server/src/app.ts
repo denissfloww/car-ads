370afe5a2ceb7ba536fab1669b7ecbfd473f5a35
denissfloww <49568323+denissfloww@users.noreply.github.com>
2021-07-08 19:08:16 +0500

    init commit + backend auth

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan'
+import 'express-async-errors';
+import middleware from './middleware';
+import authRoutes from './routes/auth';
+import userRoutes from './routes/user';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/', authRoutes);
+app.use('/users', userRoutes);
+
+app.use(middleware.unknownEndPointHandler);
+app.use(middleware.errorHandler);
+
+export default app;
