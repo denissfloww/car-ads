41fbb9049238edf4a6a26312d838440343e8a27e
bugakov_dy <denbugackoff21@gmail.com>
2021-08-27 12:02:27 +0500

    deploy on heroku

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -2,7 +2,7 @@ const type = process.env.TYPEORM_TYPE;
 const username = process.env.TYPEORM_USERNAME;
 const password = process.env.TYPEORM_PASSWORD;
 const host = process.env.TYPEORM_HOST;
-const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;;
+const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
 const database = process.env.TYPEORM_DATABASE;
 
 module.exports = {
