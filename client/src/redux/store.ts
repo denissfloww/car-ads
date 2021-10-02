85fe04d3bd36561fbbcfa2a825e6d21ce62fd1d4
denissfloww <denbugackoff21@gmail.com>
2021-10-02 09:25:48 +0500

    add notify slice and update readme

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,15 +1,16 @@
-import adSlice from "./slices/adSlice";
 import authReducer from './slices/authSlice';
 import { Action, configureStore } from '@reduxjs/toolkit';
 import { ThunkAction } from 'redux-thunk';
 import appendReducer from './slices/appendSlice';
 import adReducer from "./slices/adSlice"
+import notifyReducer from "./slices/notificationsSlice"
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     append: appendReducer,
     ad: adReducer,
+    notify: notifyReducer,
   },
 });
 
