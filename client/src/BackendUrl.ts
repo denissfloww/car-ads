bb30204ddab686db9697465efc4ccbb4dd065582
denbu <denbugackoff21@gmail.com>
2021-10-16 14:30:01 +0500

    deploy

--- client/src/BackendUrl.ts
+++ client/src/BackendUrl.ts
@@ -1,6 +1,6 @@
 // production
-// const backendUrl = 'https://ads-platform.herokuapp.com';
+const backendUrl = 'https://ads-platform.herokuapp.com';
 
 //dev
-const backendUrl = 'http://localhost:3005';
+// const backendUrl = 'http://localhost:3005';
 export default backendUrl;
